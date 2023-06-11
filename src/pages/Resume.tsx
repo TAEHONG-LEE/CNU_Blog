d26ebbdbfc749adf2a39e0068e5aa50472aeda44
TAEHONG-LEE <2t_hong@naver.com>
2023-06-11 15:25:20 +0900

    Update Resume Page

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,22 @@
 const Resume = () => {
-  return <div>프로젝트 내역인데 제발 좀 되면 좋겠</div>;
+  return (
+    <div>
+      <h1>이력서</h1>
+      <h2>프로젝트 내역</h2>
+      <ul>
+        <li>프로젝트 1: LolPAGO</li>
+        <li>프로젝트 2: CNU BLOG</li>
+        <li>프로젝트 3: Parking Service</li>
+      </ul>
+      <h2>학력</h2>
+      <ul>
+        <li>초등학교 : 당진원당초등학교</li>
+        <li>중학교 : 당진원당중학교</li>
+        <li>고등학교 : 공주한일고</li>
+        <li>대학교 : 충남대학교</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Resume;
